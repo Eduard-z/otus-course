bc9b761d2b9136b84c76e9968757141fe3a16085
Ed <chegladze@mail.ru>
2021-12-23 10:26:16 +0300

    reduced line length

--- selenium_tests/test_product_page.py
+++ selenium_tests/test_product_page.py
@@ -7,10 +7,13 @@ def test_product_page(browser):
     browser.get(url=browser.url + "/index.php?route=product/product&product_id=43")
     wait = WebDriverWait(driver=browser, timeout=2)
     # wait until button is displayed
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "input[name='product_id'] ~ button")))
+    wait.until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, "input[name='product_id'] ~ button")
+    ))
 
     browser.find_element(By.CSS_SELECTOR, ".breadcrumb a")
     browser.find_element(By.CSS_SELECTOR, ".image-additional")
     browser.find_element(By.CSS_SELECTOR, ".active > a")
-    browser.find_element(By.CSS_SELECTOR, ".btn-group > button[data-original-title='Add to Wish List']")
+    browser.find_element(By.CSS_SELECTOR,
+                         ".btn-group > button[data-original-title='Add to Wish List']")
     browser.find_element(By.CSS_SELECTOR, "[name='quantity']")
