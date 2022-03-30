7325fc0d4546ef1a2b2a4acdaefab1aa5ffc7398
Ed <chegladze@mail.ru>
2022-03-30 17:24:56 +0300

    change IP

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -13,7 +13,7 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--url", action="store", default="https://demo.opencart.com/")
     parser.addoption("--environment", action="store", default="local")
-    parser.addoption("--executor", action="store", default="192.168.100.144:4444")
+    parser.addoption("--executor", action="store", default="192.168.100.11:4444")
     parser.addoption("--browser_version", action="store", default="96.0")
     parser.addoption("--drivers", action="store",
                      default=os.path.expanduser("~/Documents/git repos/drivers"))
