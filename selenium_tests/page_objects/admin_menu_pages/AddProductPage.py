29a954989a13e9229be518e8e8bccb7230e362a2
Ed <chegladze@mail.ru>
2022-11-21 17:48:41 +0300

    updated with new methods from BasePage

--- selenium_tests/page_objects/admin_menu_pages/AddProductPage.py
+++ selenium_tests/page_objects/admin_menu_pages/AddProductPage.py
@@ -3,11 +3,11 @@ from ..BasePage import BasePage
 
 
 class AddProductPage(BasePage):
-    list_of_product_form_tabs = ['General', 'Data', 'Links', 'Attribute', 'Option',
-                                 'Recurring', 'Discount', 'Special', 'Image',
-                                 'Reward Points', 'SEO', 'Design']
+    list_of_product_form_tabs = ['General', 'Data', 'Links', 'Attribute', 'Option', 'Recurring',
+                                 'Discount', 'Special', 'Image', 'Reward Points', 'SEO', 'Design']
 
-    ADD_PRODUCT_FORM = (By.CLASS_NAME, "panel panel-default")
+    ADD_PRODUCT_FORM = (By.CLASS_NAME, "panel-body")
+    FORM_TABS_ELEMENT = (By.XPATH, "//ul[contains(@class, 'nav-tabs')]")
     PRODUCT_NAME_FIELD = (By.CSS_SELECTOR, "input[id='input-name1']")
     META_TAG_TITLE_FIELD = (By.CSS_SELECTOR, "input[id='input-meta-title1']")
     DESCRIPTION_FIELD = (By.CSS_SELECTOR, "div[role='textbox']")
@@ -17,33 +17,20 @@ class AddProductPage(BasePage):
     def wait_until_add_product_form_is_displayed(self):
         self._verify_element_presence(self.ADD_PRODUCT_FORM)
 
-    def switch_to_another_tab(self, tab_name):
-        self.browser.find_element(By.XPATH, f"//ul[contains(@class, 'nav-tabs')]"
-                                            f"//a[text()='{tab_name}']").click()
+    def switch_to_another_tab(self, tab_name: str):
+        self._click_child_element(self.FORM_TABS_ELEMENT, (By.XPATH, f"//a[text()='{tab_name}']"))
 
-    def input_product_name(self, product_name):
-        product_name_field = self._verify_element_presence(self.PRODUCT_NAME_FIELD)
-        product_name_field.click()
-        product_name_field.clear()
-        product_name_field.send_keys(product_name)
+    def input_product_name(self, product_name: str):
+        self._input_field_value(self.PRODUCT_NAME_FIELD, product_name)
 
-    def input_meta_tag_title(self, meta_tag_title):
-        meta_tag_title_field = self._verify_element_presence(self.META_TAG_TITLE_FIELD)
-        meta_tag_title_field.click()
-        meta_tag_title_field.clear()
-        meta_tag_title_field.send_keys(meta_tag_title)
+    def input_meta_tag_title(self, meta_tag_title: str):
+        self._input_field_value(self.META_TAG_TITLE_FIELD, meta_tag_title)
 
-    def input_description(self, description_text):
-        description_field = self._verify_element_presence(self.DESCRIPTION_FIELD)
-        description_field.click()
-        description_field.clear()
-        description_field.send_keys(description_text)
+    def input_description(self, description_text: str):
+        self._input_field_value(self.DESCRIPTION_FIELD, description_text)
 
-    def input_model(self, model_name):
-        model_field = self._verify_element_presence(self.MODEL_FIELD)
-        model_field.click()
-        model_field.clear()
-        model_field.send_keys(model_name)
+    def input_model(self, model_name: str):
+        self._input_field_value(self.MODEL_FIELD, model_name)
 
     def click_save_button(self):
-        self.browser.find_element(*self.SAVE_BUTTON).click()
+        self._click(self.SAVE_BUTTON)
