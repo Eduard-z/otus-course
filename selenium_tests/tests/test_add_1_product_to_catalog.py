5332a788a8eba93bd483f7f4b9bdd2d6f69cc519
Ed <chegladze@mail.ru>
2022-11-21 17:59:01 +0300

    added steps

--- selenium_tests/tests/test_add_1_product_to_catalog.py
+++ selenium_tests/tests/test_add_1_product_to_catalog.py
@@ -12,17 +12,23 @@ from .test_login_as_admin import test_login_as_admin as login_as_admin
 @allure.title("Add 1 new product to Products List")
 @pytest.mark.parametrize("test_product_name, description, meta_tag, model",
                          [("Huawei P40", "smartphone", "Huawei", "P40 Pro 5G")])
-def test_add_1_product(browser, test_product_name, description, meta_tag, model):
-    login_as_admin(browser=browser)
+def test_add_1_product_to_catalog(browser, test_product_name, description, meta_tag, model):
+    with allure.step("Pre-condition: Login as admin"):
+        login_as_admin(browser=browser)
 
     with allure.step("Open 'Add Product' page"):
         admin_page = AdminPage(browser=browser)
+        admin_page.check_admin_page_title()
         admin_page.wait_until_menu_is_displayed()
         admin_page.open_products_list()
-        admin_page.click_add_new_button()
+
+        catalog_products_page = CatalogProductsPage(browser=browser)
+        catalog_products_page.check_products_page_title()
+        catalog_products_page.click_add_new_button()
 
     with allure.step("Populate new product form"):
         add_product_page = AddProductPage(browser=browser)
+        add_product_page.wait_until_add_product_form_is_displayed()
         add_product_page.input_product_name(test_product_name)
         add_product_page.input_description(description)
         add_product_page.input_meta_tag_title(meta_tag)
